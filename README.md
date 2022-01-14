5e8180064ededdbe0ed228c0c6e6fa197f342770
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-14 09:17:24 -0600

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Deno-GLFW-Bindings
 GLFW bindings for Deno
+
+Note: This only supports Windows at this time.
---
+++ src/glfw.ts
@@ -0,0 +1,5 @@
+const glfwdll = Deno.dlopen("../glfw/glfe3.dll", {
+    //Function defs here
+})
+
+export default glfwdll
